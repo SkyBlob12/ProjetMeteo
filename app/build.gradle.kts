12ea6525471a0a4df8c86d41f4001a2f73cb1d11
Clement <clement.andrieu@ynov.com>
2023-12-05 16:16:15 +0100

    Mise en forme de la page d'accueil

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.projetmeteo"
+    namespace = "com.example.appelhttp"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.projetmeteo"
+        applicationId = "com.example.appelhttp"
         minSdk = 30
         targetSdk = 33
         versionCode = 1
@@ -36,6 +36,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.android.volley:volley:1.2.1")
+    implementation ("com.squareup.picasso:picasso:2.8")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
